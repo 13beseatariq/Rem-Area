a297ff0860536d5919f6c8c576be48205d77da97
Ali Azlan <aliazlan4@gmail.com>
2016-05-04 01:42:06 +0500

    map activity added

--- app/src/main/java/com/remarea/alitariq/remarea/userHome.java
+++ app/src/main/java/com/remarea/alitariq/remarea/userHome.java
@@ -1,11 +1,15 @@
 package com.remarea.alitariq.remarea;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class userHome extends AppCompatActivity {
+public class userHome extends AppCompatActivity implements View.OnClickListener {
     private String username;
+    Button addReminder, checkReminders;
 
     TextView welcomeUsername;
     @Override
@@ -14,6 +18,11 @@ public class userHome extends AppCompatActivity {
         setContentView(R.layout.activity_user_home);
 
         welcomeUsername = (TextView) findViewById(R.id.welcomeUsername);
+        addReminder = (Button) findViewById(R.id.addReminder);
+        checkReminders = (Button) findViewById(R.id.checkReminders);
+
+        addReminder.setOnClickListener(this);
+        checkReminders.setOnClickListener(this);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -21,5 +30,15 @@ public class userHome extends AppCompatActivity {
         }
 
         welcomeUsername.setText("Welcome: " + username);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == addReminder){
+            this.startActivity(new Intent(this, addReminderMap.class));
+        }
+        else if(v == checkReminders){
+
+        }
     }
 }
